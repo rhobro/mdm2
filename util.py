dbd56f16259c5e8ea151f983458732c97d194047
Rohan Mathew <robozmat@gmail.com>
2025-03-18 13:52:24 +0000

    cw dtype data

--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+from random import randint
+
+import numpy as np
 import pandas as pd
 
 PATH = "data/clean.csv"
@@ -18,3 +21,17 @@ def load_with_columns(cols=None):
 
     print(f"Datapoints: {rows_before} --> {rows_after} ({rows_before - rows_after} removed)")
     return sheet[cols]
+
+def summarise_cnf(cnf):
+    accuracy = np.diagonal(cnf).sum() / cnf.sum()
+    print(f"Accuracy: {accuracy}")
+    precision = cnf[1, 1] / cnf[:, 1].sum()
+    print(f"Precision: {precision}")
+    sensitivity = cnf[1, 1] / cnf[1].sum()
+    print(f"Sensitivity: {sensitivity}")
+    specificity = cnf[0, 0] / cnf[0].sum()
+    print(f"Specificity: {specificity}")
+    print(cnf)
+
+def rand_seed():
+    return randint(0, 1000)
